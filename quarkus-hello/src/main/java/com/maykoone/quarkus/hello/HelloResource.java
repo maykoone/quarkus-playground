305cd9cf1cf920109cfd97051bc164e05db62d27
Mayko B. Oliveira <maykoone@gmail.com>
2020-05-17 22:19:42 -0300

    Externalized greeting message

--- quarkus-hello/src/main/java/com/maykoone/quarkus/hello/HelloResource.java
+++ quarkus-hello/src/main/java/com/maykoone/quarkus/hello/HelloResource.java
@@ -8,12 +8,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello")
 public class HelloResource {
+
+    @ConfigProperty(name = "grettings.message")
+    String message;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello. Today is " + DateFormat.getDateTimeInstance().format(new Date()) + ". Node: " + System.getenv().getOrDefault("HOSTNAME", "unknown");
+        return message + ". Today is " + DateFormat.getDateTimeInstance().format(new Date()) + ". Node: " + System.getenv().getOrDefault("HOSTNAME", "unknown");
     }
 }
